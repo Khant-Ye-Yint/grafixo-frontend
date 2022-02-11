7b4a6e2ed94f47778e5bdee874022d09c6c5fb0f
Khant Ye Yint <khantyeyint13@gmail.com>
2022-02-11 23:07:57 +0630

    Modal design update

--- components/portfolio/Modal.js
+++ components/portfolio/Modal.js
@@ -10,8 +10,11 @@ function Modal({ modalShow, setModalShow }) {
 	}, []);
 
 	const modalContent = modalShow ? (
-		<div className='fixed top-0 left-0 bottom-0 right-0 w-full h-full flex justify-center items-center bg-black z-50 bg-opacity-50'>
-			<div className='w-96 bg-darkGray text-black tracking-wide'>
+		<div className='fixed top-0 left-0 bottom-0 right-0 w-full h-full flex justify-center items-center bg-black z-50 bg-opacity-50 '>
+			<div
+				className=' bg-darkGray text-black tracking-wide'
+				style={{ width: '63rem' }}
+			>
 				<div className='flex flex-row justify-between items-center px-8 my-4'>
 					<h1 className='text-xl font-bold font-bree text-primary'>Polaroid</h1>
 					<button
@@ -23,22 +26,23 @@ function Modal({ modalShow, setModalShow }) {
 				</div>
 				{/* <div className='h-56 bg-gray-400'> */}
 				<ReactPlayer
-					url={'https://www.youtube.com/watch?v=dQw4w9WgXcQ'}
-					height={'14rem'}
-					width={'24rem'}
+					url={'https://www.youtube.com/watch?v=mgRs_Dwu_ZY'}
+					width='full'
+					height={'35rem'}
 					controls
 				/>
 
-				<div className='px-8 my-4'>
-					<h1 className='text-medium font-bold font-bree text-white'>
-						Polaroid
-					</h1>
+				<div className='px-8 my-5'>
 					<h3 className='text-normal  font-bree text-white'>
 						Company - Astra Shark
 					</h3>
-					<p className='text-testamonial font-montserrat text-lightGray'>
+					<span className='text-testamonial font-montserrat text-green-300 font-bold'>
+						$ 300
+					</span>
+					<br />
+					<span className='text-testamonial font-montserrat text-lightGray'>
 						3 June, 2021
-					</p>
+					</span>
 					<p className='text-testamonial font-montserrat text-white mt-3'>
 						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
 						laoreet a massa, risus. At felis, posuere eu pretium. Aliquet mauris
--- components/sections/Footer.js
+++ components/sections/Footer.js
@@ -11,54 +11,54 @@ import TwitterLogo from '../../public/images/logo/twitter.png';
 
 const Footer = () => {
 	return (
-		<div className='w-full grid place-items-center  bg-darkGray laptop:pt-contain laptop:px-footer tablet:pt-tabletContain tablet:px-tabletContain phone:px-phone phone:pt-10 text-lightGray font-montserrat relative'>
-			<div className='flex tablet:w-full phone:w-2/3 tablet:flex-row tablet:justify-between laptop:flex-row phone:flex-col items-start laptop:justify-between laptop:items-center phone:space-y-16 laptop:space-y-0 tablet:space-y-0 '>
-				<div className='flex flex-col space-y-2'>
-					<Link href='/'>
-						<div className='h-10 w-full relative cursor-pointer'>
+		<div className="w-full grid place-items-center  bg-darkGray laptop:pt-contain laptop:px-footer tablet:pt-tabletContain tablet:px-tabletContain phone:px-phone phone:pt-10 text-lightGray font-montserrat relative">
+			<div className="flex tablet:w-full phone:w-2/3 tablet:flex-row tablet:justify-between laptop:flex-row phone:flex-col items-start laptop:justify-between laptop:items-center phone:space-y-16 laptop:space-y-0 tablet:space-y-0 ">
+				<div className="flex flex-col space-y-2">
+					<Link href="/">
+						<div className="h-10 w-full relative cursor-pointer">
 							<Image
 								src={Logo}
-								layout='fill'
-								objectFit='contain'
-								objectPosition='left'
+								layout="fill"
+								objectFit="contain"
+								objectPosition="left"
 							/>
 						</div>
 					</Link>
 					<span>+959 765 203 468</span>
 					<span>grafixostudio@gmail.com</span>
 				</div>
-				<div className='flex flex-col space-y-2'>
-					<h1 className='text-primary text-medium font-bold'>LINKS</h1>
-					<FooterLink text='3D Modeling And Rendering' link='' />
-					<FooterLink text='3D Product Animation' link='' />
-					<FooterLink text='3D/2D Promotional Animation' link='' />
+				<div className="flex flex-col space-y-2">
+					<h1 className="text-primary text-medium font-bold">LINKS</h1>
+					<FooterLink text="3D Modeling And Rendering" link="" />
+					<FooterLink text="3D Product Animation" link="" />
+					<FooterLink text="3D/2D Promotional Animation" link="" />
 				</div>
-				<div className='flex flex-col space-y-2'>
-					<h1 className='text-primary text-medium font-bold'>INFO</h1>
-					<FooterLink text='About Us' link='' />
-					<FooterLink text='Contact Us' link='' />
+				<div className="flex flex-col space-y-2">
+					<h1 className="text-primary text-medium font-bold">INFO</h1>
+					<FooterLink text="About Us" link="" />
+					<FooterLink text="Contact Us" link="" />
 				</div>
 			</div>
-			<div className='w-full h-20  mt-28 flex flex-row justify-between items-center border-t border-gray-500'>
-				<span className='text-small'>Copyright © 2022</span>
-				<div className='flex flex-row items-center space-x-5'>
-					<span className='cursor-pointer'>
-						<Link href=''>
+			<div className="w-full h-20  mt-28 flex flex-row justify-between items-center border-t border-gray-500">
+				<span className="text-small">Copyright © 2022</span>
+				<div className="flex flex-row items-center space-x-5">
+					<span className="cursor-pointer">
+						<Link href="">
 							<Image src={FacebookLogo} />
 						</Link>
 					</span>
-					<span className='cursor-pointer'>
-						<Link href=''>
+					<span className="cursor-pointer">
+						<Link href="">
 							<Image src={MailLogo} />
 						</Link>
 					</span>
-					<span className='cursor-pointer'>
-						<Link href=''>
+					<span className="cursor-pointer">
+						<Link href="">
 							<Image src={InstagramLogo} />
 						</Link>
 					</span>
-					<span className='cursor-pointer'>
-						<Link href=''>
+					<span className="cursor-pointer">
+						<Link href="">
 							<Image src={TwitterLogo} />
 						</Link>
 					</span>
